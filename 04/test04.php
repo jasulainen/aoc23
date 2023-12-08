12b6d31e04b060e3cdb7c02d46006117c0906c37
Jasulainen <jasmohiltula@gmail.com>
2023-12-08 16:10:48 +0000

    Day 4, phase 2 done

--- 04/test04.php
+++ 04/test04.php
@@ -2,8 +2,9 @@
 
 $file = file("input.txt");
 $total_sum = 0;
+$card_amount = array_fill(0,count($file),1);
 
-foreach($file as $row)
+foreach($file as $key => $row)
 {
 	$row = trim($row);
 	$row = explode(":", $row)[1];
@@ -19,12 +20,18 @@ foreach($file as $row)
 			$amount_of_wins++;
 		}
 	}
-	if($amount_of_wins > 0)
-		$total_sum += pow(2, $amount_of_wins-1);
 	
-	print $total_sum . PHP_EOL;
-
+	if($amount_of_wins > 0)
+	{
+		for($i=$key+1; $i<=$amount_of_wins+$key; $i++)
+		{
+			$card_amount[$i]+=$card_amount[$key];
+		}
+	}
 }
+
+print_r($card_amount);
+print array_sum($card_amount) . PHP_EOL;
 
 function readNumbers($row)
 {
